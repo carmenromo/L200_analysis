6fcb1572132f8e5b20cb907209a628f50dcda38a
carmenromo <carmenromoluque@gmail.com>
2023-09-21 16:06:19 -0600

    Fix script and add parse functions file

--- bsm_low_thr/build_LEGEND_raw_wfs.py
+++ bsm_low_thr/build_LEGEND_raw_wfs.py
@@ -1,22 +1,11 @@
 import sys
-import argparse
+
+from parse_functions import parse_args
 
 from daq2lh5 import build_raw
 from parse   import parse
 
-def parse_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('period'       , type = int,                                help = "LEGEND data period"   )
-    parser.add_argument('run'          , type = int,                                help = "LEGEND run number"    )
-    parser.add_argument('data_type'    , type = str,                                help = "Data type: tst or cal")
-    parser.add_argument('in_dir'       , type = str,                                help = "Input files path"     )
-    parser.add_argument('filename'     , type = str,                                help = "Name of input files"  )
-    parser.add_argument('out_dir'      , type = str,                                help = "Output files path"    )
-    parser.add_argument('ifiles_format', type = str, default = "orca",              help = "Input files format"   )
-    parser.add_argument('json_file'    , type = str, default = "2ge_all_spms.json", help = "Json file"            )
-    return parser.parse_args()
-
-arguments     = pf.parse_args(sys.argv)
+arguments     = parse_args(sys.argv)
 period        = arguments.period
 run           = arguments.run
 data_type     = arguments.data_type
@@ -40,6 +29,7 @@ print(run, in_dir, out_dir)
 
 f_parse  = parse(file_format, filename)
 #if not f_parse: continue
+in_file  = in_dir + filename
 out_file = out_dir + 'raw_wf_' + f_parse['datetime'] + '.lh5'
 
 build_raw(in_file,
